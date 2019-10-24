cb57b726e6918b21bc28b92ac11cf5126925357d
jeremy <jeremy@golangdev.jeremycoando.com>
2019-10-24 07:13:42 -0600

    coolage

--- main.go
+++ main.go
@@ -1,45 +1,44 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	p1 "github.com/pseudonative/goStuffAll/animals"
-
-	"github.com/pseudonative/goStuffAll/shapes1"
-	"github.com/pseudonative/goStuffAll/shapes2"
-)
+type player struct {
+	name, position, sport, team string
+	age                         int
+}
 
 func main() {
-	testCase1()
-	testCase2()
-	// testCase3()
+	player1 := player{"KobeBryant", "ShootingGuard", "Basketball", "LALakers", 41}
+	fmt.Println("Player1=", player1)
+	fmt.Printf("(1) name=%s position=%s sport=%s team=%s age=%d\n\n",
+		player1.name, player1.position, player1.sport, player1.team, player1.age)
 
+	player2 := player{
+		name:     "DezBryant",
+		position: "WideReceiver",
+		sport:    "Football",
+		team:     "FreeAgent(DallasCowboys)",
+		age:      30,
+	}
+	fmt.Println("Player2=", player2)
+	fmt.Printf("(2) name=%s position=%s sport=%s team=%s age=%d\n\n",
+		player2.name, player2.position, player2.sport, player2.team, player2.age)
+
+	var player3 player
+	player3.name = "MichaelJordan"
+	player3.position = "ShootingGuard"
+	player3.sport = "Basketball"
+	player3.team = "Retired(ChicagoBulls)"
+	player3.age = 56
+	fmt.Println("Player3", player3)
+	fmt.Printf("Player3=%+v\n", player3)
+
+	player4 := new(player)
+	player4.name = "ScottyPippen"
+	player4.position = "Foreward"
+	player4.sport = "Basketball"
+	player4.team = "Retired(ChicagoBulls)"
+	player4.age = 53
+	fmt.Printf("&Player4=%v\n", *player4)
+	fmt.Printf("&Player4=%+v\n", *player4)
 }
-func testCase1() {
-	fmt.Printf("shape1 pkg - Rectangle Area= %d\n", shapes1.Area(shapes1.Length, shapes1.Width))
-	fmt.Printf("shape1 pkg - Square Area= %d\n", shapes1.SquareArea(2))
-	fmt.Printf("shape2 pkg - Rectangle Area= %d\n", shapes2.Area(2, 3))
-	fmt.Printf("shape2 pkg - Square Area= %d\n", shapes2.SquareArea(2))
-
-}
-
-func testCase2() {
-	lion := p1.Lion{}
-	fmt.Println(lion.Speaks())
-
-	cat := p1.Cat{}
-	fmt.Println(cat.Speaks())
-
-	human := p1.Human{}
-	fmt.Println(human.Speaks())
-}
-
-// func testCase3() {
-
-// }
-// func changeAthleteName1(p athletes.Player) {
-
-// }
-// func changeAthleteName2(p *athletes.Player) {
-
-// }
